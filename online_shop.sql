a38394a5bc4e124ddd77688b101717702ee2010c
Tolkinxon <krosstony639@gmail.com>
2025-06-16 16:02:52 +0500

    Finish all tables

--- online_shop.sql
+++ online_shop.sql
@@ -46,3 +46,32 @@ CREATE TABLE IF NOT EXISTS products(
 INSERT INTO products(name, price, category_id, product_img) VALUES
 ("Palto", "1000000", 1, 'https://picsum.photos/200/300');
 
+CREATE TABLE IF NOT EXISTS orders(
+    id INT AUTO_INCREMENT,
+    product_id INT,
+    user_id INT,
+    amount INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(product_id) REFERENCES products(id)
+);
+
+INSERT INTO orders(product_id, user_id, amount) VALUES
+(1, 1, 3),
+(1, 1, 2);
+
+CREATE TABLE IF NOT EXISTS purchased(
+    id INT AUTO_INCREMENT,
+    product_id INT,
+    user_id INT,
+    amount INT,
+    bought_date DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(product_id) REFERENCES products(id)
+);
+INSERT INTO purchased(product_id, user_id, amount, bought_date) VALUES
+(1, 1, 3, CURRENT_DATE),
+(1, 1, 2, CURRENT_DATE);
