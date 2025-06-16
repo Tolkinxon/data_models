35d90068a489a8498b576da485c465a8c78458b6
Tolkinxon <krosstony639@gmail.com>
2025-06-16 15:49:26 +0500

    add new table

--- online_shop.sql
+++ online_shop.sql
@@ -3,5 +3,46 @@ CREATE DATABASE IF NOT EXISTS online_shop ;
 USE online_shop;
 
 CREATE TABLE IF NOT EXISTS  users(
-    
+    id int AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN,
+    address VARCHAR(400) NOT NULL,
+    postcode VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
 ); 
+
+INSERT INTO users(first_name, last_name,  password, email,  phone_number, is_admin, address, postcode ) VALUES
+("To'llqinxon", "Soliyev", "jifdHU%^4", "tolkinxon@gmail.com", "+998774779844", true, "Jomboy tuman/Samarqand MFY", "04070");
+
+CREATE TABLE IF NOT EXISTS categories(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO categories(name) VALUES
+('Erkaklar kiyimi');
+
+CREATE TABLE IF NOT EXISTS products(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price  DECIMAL(10,2) NOT NULL,
+    category_id INT,
+    product_img VARCHAR(2000),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    FOREIGN KEY(category_id) REFERENCES categories(id)
+);
+
+INSERT INTO products(name, price, category_id, product_img) VALUES
+("Palto", "1000000", 1, 'https://picsum.photos/200/300');
+
